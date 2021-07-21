3ed7132680bc1e79ac21f8d79e22fc2843d1bd33
Divyanshu Parihar <divyanshu1447@gmail.com>
2021-07-21 10:51:18 +0530

    insertion sort

--- Arrays/sorting.cpp
+++ Arrays/sorting.cpp
@@ -16,6 +16,18 @@ void bubbleSort(int *arr, int n){
 }
 
 
+void insertionSort(int *arr, int  n){
+    for (int i = 1; i<n;i++){
+        int current = arr[i];
+        int j = i -1;
+        while(arr[j] > current && j >= 0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+}
+
 void selectionSort(int *arr,int n){
     //  5 7 3 8 2 -> min 2
     // cout<< 'here';
